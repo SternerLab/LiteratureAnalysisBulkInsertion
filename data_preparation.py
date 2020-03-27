ceb75580d394ae87a7e49bd0aac15ec8383f97cc
Thek10patil <kkpatil1@asu.edu>
2020-03-27 01:30:10 -0700

    Adding Data preparation template

---
+++ data_preparation.py
@@ -0,0 +1,11 @@
+class DataPreparation:
+    def __init__(self):
+        pass
+
+    # TODO: Process term vector using multiprocessing
+    def term_vector_processing(self, term_vector_json):
+        pass
+
+
+if __name__ == "__main__":
+    data_preparation = DataPreparation()
