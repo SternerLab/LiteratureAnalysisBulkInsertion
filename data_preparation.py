5c15acbda595ba91f7f57d30c70fe6ce2f7edba7
Thek10patil <kkpatil1@asu.edu>
2020-03-28 18:46:06 -0700

    Changes made in manager dic

--- data_preparation.py
+++ data_preparation.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import utils
 import json
 import multiprocessing
@@ -29,44 +31,47 @@ def get_cpu_count():
     return cpu_count
 
 
-def term_process(term_list, t_counter, term, term_dict):
+def term_process(shared_doc_dict, index, t_counter, term, term_dict):
     # print("Process number:- {0} started".format(t_counter))
     gram = len(term.split(" "))
     term_count = term_dict["term_freq"]
     return_dict = {"term": term, "term_count": term_count, "gram": gram}
-    term_list.append(return_dict)
-    if t_counter % 100 == 0:
-        print("Process number:- {0} Completed".format(t_counter))
+    shared_doc_dict[index]["term_vectors"]["terms"].append(return_dict)
+    # if t_counter % 100 == 0:
+    #     print("Process number:- {0} Completed".format(t_counter))
 
 
 # TODO: Process term vector using multiprocessing
-def term_vector_processing(term_vector, term_list):
-    cpu_count = get_cpu_count()
-    pool = multiprocessing.Pool(cpu_count)
+def term_vector_processing(term_vector, shared_doc_dict, index):
+
     if "term_vectors" in term_vector:
+        cpu_count = get_cpu_count()
+        pool = multiprocessing.Pool(cpu_count)
         terms = term_vector["term_vectors"]["plain_text"]["terms"]
         term_counter = 0
         for term in terms:
-            # print term
-            pool.apply_async(term_process, args=(term_list, term_counter, term, terms[term]))
+            shared_doc_dict[index]["term_vectors"]["terms"] = list()
+            pool.apply_async(term_process, args=(shared_doc_dict, index, term_counter, term, terms[term]))
             term_counter += 1
-
         pool.close()
         pool.join()
-        return_term_vector = {"field_statistics": term_vector["term_vectors"]["plain_text"]["field_statistics"],
-                              "terms": list(term_list)}
-        return return_term_vector
+        shared_doc_dict[index]["term_vectors"]["field_statistics"] = term_vector["term_vectors"]["plain_text"][
+            "field_statistics"]
     return {}
 
 
-def process_doc(document, shared_doc_list, terms, shared_terms):
+def process_doc(document, shared_doc_dict, index, terms):
+    print(shared_doc_dict)
+    # shared_doc_dict[index]["term_vectors"] = defaultdict()
+    # term_vector_processing(terms, shared_doc_dict, index)
     new_doc_dict = {"year": document["_source"]["article"]["article-meta"]["year"],
                     "article-id": document["_source"]["article"]["article-meta"]["article-id"],
                     "journal-id": document["_source"]["article"]["journal-meta"]["journal-id"],
                     "journal-title": document["_source"]["article"]["journal-meta"]["journal-title"],
-                    # "plain_text": document["_source"]["plain_text"],
-                    "term_vectors": term_vector_processing(terms, shared_terms)}
-    shared_doc_list.append(new_doc_dict)
+                    # "plain_text": document["_source"]["plain_text"]}
+                    }
+    print(shared_doc_dict)
+    shared_doc_dict[index] = new_doc_dict
 
 
 def get_termvectors_for_doc(elasticsearch_client, doc_id, fields=['plain_text'], ):
@@ -82,22 +87,21 @@ def process(elasticsearch_client):
     is_done = False
     offset = 0
     # TODO: Processing limit number of records each time
-    limit = 20
+    limit = 1
     while not is_done:
         fetched_docs = elasticsearch_client.search(index="beckett_jstor_ngrams_part", doc_type='article', size=limit,
                                                    from_=offset)
         fetched_docs = fetched_docs["hits"]["hits"]
         process = []
-        shared_doc_list = manager.list()
-        shared_term_dict = manager.dict()
+        shared_doc_dict = manager.dict()
         for index, doc in enumerate(fetched_docs):
-            shared_term_dict[index] = manager.list()
             terms = elasticsearch_client.termvectors(index="beckett_jstor_ngrams_part", id=doc["_id"], offsets=False,
                                                      fields=["plain_text"],
                                                      positions=False, payloads=False)
-            # shared_terms = term_vector_processing(terms, shared_terms_list)
+            shared_doc_dict[index] = defaultdict()
             process.append(Process(target=process_doc,
-                                   args=(doc, shared_doc_list, terms, shared_term_dict[index])))
+                                   args=(
+                                       doc, shared_doc_dict, index, terms)))
             print("process {} started".format(index))
             process[index].start()
 
@@ -107,9 +111,9 @@ def process(elasticsearch_client):
         if len(fetched_docs) < limit:
             is_done = True
         break
-    utils.json_file_writer("./", "result.json", json.dumps(list(shared_doc_list)))
+    utils.json_file_writer("./", "result.json", json.dumps(list(shared_doc_dict.values())))
     # print(list(shared_doc_list))
-    print(len(list(shared_doc_list)))
+    print(len(list(shared_doc_dict.values())))
 
 
 if __name__ == "__main__":
